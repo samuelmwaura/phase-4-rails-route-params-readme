187875daf29ac8d5f5f097ff16b6c90ba7bea0df
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-09-28 17:22:57 +0300

    commit tree b4ac4c2c888a167c9fc7c85d242e88d8a107a0a4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get '/cheeses', to: 'cheeses#index'
+  get 'cheeses', to: 'cheeses#index' #the index is the method in the controller that runs when this route ins invoked
+  get 'cheeses/:id', to: 'cheeses#show' #Here we turn to dynamic routes. Our show method is dynamic too and renders details of the app whose address is passed asa parameter to the url
 end
